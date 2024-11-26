076773cd20723c5fc9d73825fabc10e75458dba5
Pedro Benetti Mestrinari <pedromestrinari@gmail.com>
2024-11-26 18:22:27 -0300

    Just some tweaks

--- nvim/.config/nvim/lua/mestrinari/keymaps.lua
+++ nvim/.config/nvim/lua/mestrinari/keymaps.lua
@@ -7,6 +7,9 @@ vim.keymap.set('n', "<leader>e", vim.cmd.Exp)
 -- Neogit
 vim.keymap.set('n', "<C-z>", vim.cmd.Neogit)
 
+-- Mason
+vim.keymap.set('n', "<leader>m", vim.cmd.Mason)
+
 -- Move lines in Visual Mode
 vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
@@ -18,5 +21,4 @@ vim.api.nvim_set_keymap("n", "<leader>q", ":!chmod +x %<CR>", {})
 vim.api.nvim_set_keymap("n", "<leader>w", ":!%:p %<CR>", {})
 
 -- Change terminal escape Key
-vim.keymap.set('t', '<C-e>', '<C-\\><C-n><CR>')
-
+vim.keymap.set('t', '<C-w>', '<C-\\><C-n><CR>')
--- nvim/.config/nvim/lua/mestrinari/packages.lua
+++ nvim/.config/nvim/lua/mestrinari/packages.lua
@@ -1,96 +1,94 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-local uv = vim.uv or vim.loop
-
--- Auto-install lazy.nvim if not present
-if not uv.fs_stat(lazypath) then
-  print('Installing lazy.nvim....')
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
-  print('Done.')
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
-
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-  { -- Colorscheme
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-  { -- Mason
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim'
-  },
-  { -- LSP Support
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    lazy = true,
-    config = false,
-  },
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      { 'hrsh7th/cmp-nvim-lsp' },
-    }
-  },
-  { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      { 'L3MON4D3/LuaSnip' },
-      { 'saadparwaiz1/cmp_luasnip' },
-      { 'rafamadriz/friendly-snippets' }
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    { -- Mason
+      'williamboman/mason.nvim',
+      'williamboman/mason-lspconfig.nvim'
+    },
+    { -- LSP Support
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v3.x',
+      lazy = true,
+      config = false,
+    },
+    {
+      'neovim/nvim-lspconfig',
+      dependencies = {
+        { 'hrsh7th/cmp-nvim-lsp' },
+      }
+    },
+    { -- Autocompletion
+      'hrsh7th/nvim-cmp',
+      dependencies = {
+        { 'L3MON4D3/LuaSnip' },
+        { 'saadparwaiz1/cmp_luasnip' },
+        { 'rafamadriz/friendly-snippets' }
+      },
+    },
+    { -- Treesitter
+      'nvim-treesitter/nvim-treesitter',
+    },
+    {
+      'nvim-java/nvim-java'
+    },
+    { -- Telescope
+      'nvim-telescope/telescope.nvim',
+      tag = '0.1.5',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    { -- Undotree
+      "jiaoshijie/undotree",
+      dependencies = "nvim-lua/plenary.nvim",
+      config = true,
+      keys = { -- load the plugin only when using it's keybinding:
+        { "<F5>", "<cmd>lua require('undotree').toggle()<cr>" },
+      },
+    },
+    { -- Neogit
+      "NeogitOrg/neogit",
+      dependencies = {
+        "sindrets/diffview.nvim", -- optional - Diff integration
+      },
+      config = true
+    },
+    { -- Autopairs
+      'windwp/nvim-autopairs',
+      event = "InsertEnter",
+      opts = {} -- this is equalent to setup({}) function
+    },
+    {           -- Colorizer
+      'norcalli/nvim-colorizer.lua',
+    },
+    { -- Comment
+      'numToStr/Comment.nvim'
     },
   },
-  { -- Treesitter
-    'nvim-treesitter/nvim-treesitter',
-  },
-  { -- Telescope
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  { -- Undotree
-    "jiaoshijie/undotree",
-    dependencies = "nvim-lua/plenary.nvim",
-    config = true,
-    keys = { -- load the plugin only when using it's keybinding:
-      { "<F5>", "<cmd>lua require('undotree').toggle()<cr>" },
-    },
-  },
-  { -- Neogit
-    "NeogitOrg/neogit",
-    dependencies = {
-      "sindrets/diffview.nvim", -- optional - Diff integration
-    },
-    config = true
-  },
-  { -- Autopairs
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    opts = {} -- this is equalent to setup({}) function
-  },
-  {           -- Colorizer
-    'norcalli/nvim-colorizer.lua',
-  },
-  { -- Comment
-    'numToStr/Comment.nvim'
-  },
-  { -- Markdown Preview
-  "toppair/peek.nvim",
-  event = { "VeryLazy" },
-  build = "deno task --quiet build:fast",
-  config = function()
-    require("peek").setup()
-    -- refer to `configuration to change defaults`
-    vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-    vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-  end,
-  }
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
 })
